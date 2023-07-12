4f95ae8d6a9a41374c49ce57324c99a4039b1e2c
H33Kz <qlmejk@gmail.com>
2023-07-12 16:21:30 +0200

    Added controller and html template

--- src/main/java/com/h33kz/CovidStatsSummary/controllers/GUIController.java
+++ src/main/java/com/h33kz/CovidStatsSummary/controllers/GUIController.java
@@ -1,13 +1,21 @@
 package com.h33kz.CovidStatsSummary.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.h33kz.CovidStatsSummary.repository.SimpleDataRepository;
 
 @Controller
 public class GUIController {
+  @Autowired
+  private SimpleDataRepository repo;
+
   @GetMapping("/")
-  public String getHome(){
-    return "test";
+  public ModelAndView getHome(){
+    ModelAndView mav = new ModelAndView("index");
+    mav.addObject("countries", repo.findAll());
+    return mav;
   }
-  
 }
